4a208879ce9ec4920acaf432d4122ed9cf5159b3
atrgv <rakshit@atrgv.org>
2026-04-12 21:17:06 -0700

    tested few different functions with rk and they work

--- rk.py
+++ rk.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 step_size = 0.01
-y_0 = 1.0
-t_0 = 0.5 
-t_final = 5.0
+y_0 = 0.1
+t_0 = 0.1 
+t_final = 10
 num_iter = 0 
 
 # dy/dt = y^2 
 def f(t, y):
-    return y*y 
+    return 2*t 
 
 y = [y_0]
 t = [t_0]
@@ -22,18 +22,19 @@ while (t[-1] < t_final):
 
     t_next = t[-1] + step_size
     y_next = y[-1] + step_size / 6 * (k_1 + 2*k_2 + 2*k_3 + k_4)
-    print(f"{t_next}, {y_next}")
+    # print(f"{t_next}, {y_next}")
     t.append(t_next)
     y.append(y_next)
 
     num_iter += 1 
 
-def y_solved(t):
-    return 1.0/(1.5-t)
-t_arr = np.array(t)
+# def y_solved(t):
+#     return 1.0/(1.5-t)
+# t_arr = np.array(t)
 
 plt.plot(t, y, label="rk-4")
-plt.plot(t_arr, y_solved(t_arr), label="exact", linestyle="dashed")
+# plt.plot(t_arr, y_solved(t_arr), label="exact", linestyle="dashed")
 plt.grid()
 plt.legend()
 plt.savefig("./rk.png")
+print(num_iter)
