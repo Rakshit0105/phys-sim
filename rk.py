736c365b32ca00bdd8300a41d47c0d253f708517
atrgv <rakshit@atrgv.org>
2026-04-13 00:38:27 -0700

    modified rk.py to have rk_single that returns just a single next step

--- rk.py
+++ rk.py
@@ -9,7 +9,6 @@ def rk(f, t_i, y_i, t_f, h=0.1):
 
     y = [y_0]
     t = [t_0]
-    iter = 0 # number of iteratinos
 
     while (t[-1] < t_final):
         k_1 = f(t[-1], y[-1])
@@ -22,6 +21,21 @@ def rk(f, t_i, y_i, t_f, h=0.1):
         t.append(t_next)
         y.append(y_next)
     
-        iter += 1
-
     return t, y, iter
+
+
+def rk_single(f, t_i, y_i, t_f, h=0.1)
+    y = y_i
+    t = t_i
+    t_final = t_f
+    step_size = h 
+
+    k_1 = f(t, y)
+    k_2 = f(t+step_size/2, y+k_1*step_size/2)
+    k_3 = f(t+step_size/2, y+k_2*step_size/2)
+    k_4 = f(t+step_size, y+k_3*step_size)
+
+    t_next = t + step_size
+    y_next = y + step_size / 6 * (k_1 + 2*k_2 + 2*k_3 + k_4)
+    
+    return t_next, y_next
